667c78ba06333ce644fa0df7f75c16d0a5fe7e34
KenjiNakamura <kenji.nkmr.1117@gmail.com>
2021-09-30 13:07:11 +0900

    articles search

--- app/Http/Controllers/Api/ArticlesController.php
+++ app/Http/Controllers/Api/ArticlesController.php
@@ -13,9 +13,13 @@ class ArticlesController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $articles = Article::all();
+        // $articles = Article::all();
+
+        // search
+        $articles = Article::search($request);
+
         return $articles;
     }
 
--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
     use HasFactory;
+
+    public static function search(Request $request)
+    {
+        $query = self::query();
+        if (!empty($request->freeword)) {
+            $query->where('title', 'like', '%' . $request->freeword . '%');
+        }
+
+        return $query->orderBy('id', 'desc')->paginate(10);
+    }
 }
