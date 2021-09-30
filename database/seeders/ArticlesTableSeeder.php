dc5c432205d0929e735c1c42853e14d5f0298b3d
KenjiNakamura <kenji.nkmr.1117@gmail.com>
2021-09-30 13:07:01 +0900

    fix seeder

--- database/seeders/ArticlesTableSeeder.php
+++ database/seeders/ArticlesTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Article;
 
 class ArticlesTableSeeder extends Seeder
 {
@@ -13,20 +14,6 @@ class ArticlesTableSeeder extends Seeder
      */
     public function run()
     {
-        // articlesテーブルにデータをinsert
-        \DB::table('articles')->insert([
-            [
-                'title' => 'タイトル1',
-                'body' => '内容1'
-            ],
-            [
-                'title' => 'タイトル2',
-                'body' => '内容2'
-            ],
-            [
-                'title' => 'タイトル3',
-                'body' => '内容3'
-            ],
-        ]);
+        Article::factory()->count(30)->create();
     }
 }
